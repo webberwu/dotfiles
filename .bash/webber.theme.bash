506a9201b5225ac84a056025d6c3724b76b22c5f
Webber Wu <chenshin0719@gmail.com>
2018-12-14 10:10:58 +0800

    add arrows to ps1 prompt

--- .bash/webber.theme.bash
+++ .bash/webber.theme.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 theme_main_color="\[\e[38;5;15m\]"
 theme_main_bgcolor="\[\e[48;5;162m\]"
+theme_main_arrow="\[\e[38;5;162m\]\[\e[48;5;238m\]"
 theme_second_color="\[\e[0;37m\]"
 theme_second_bgcolor="\[\e[48;5;238m\]"
+theme_second_arrow="\[\e[38;5;238m\]\[\e[48;5;16m\]"
 theme_third_color="\[\e[38;5;208m\]"
 theme_highlight="\[\e[38;5;162m\]\[\e[48;5;238m\]"
 theme_ps1_suffix="\[\e[0;90m\]»\[\e[0;37m\]»\[\e[0;97m\]» \[\e[0m\]"
@@ -25,7 +27,7 @@ function git_prompt_info {
 }
 
 function prompt_command() {
-    PS1="${theme_main_color}${theme_main_bgcolor} \u ${theme_second_color}${theme_second_bgcolor} \h[${theme_highlight}\j${theme_second_color}${theme_second_bgcolor}] \t ${normal}${theme_third_color} \w$(scm_prompt_info) ${theme_ps1_suffix}"
+    PS1="${theme_main_color}${theme_main_bgcolor} \u ${theme_main_arrow}${theme_second_color}${theme_second_bgcolor} \h[${theme_highlight}\j${theme_second_color}${theme_second_bgcolor}] \t ${theme_second_arrow}${normal}${theme_third_color} \w$(scm_prompt_info) ${theme_ps1_suffix}"
 }
 
 safe_append_prompt_command prompt_command
---
+++ README.md
@@ -0,0 +1,13 @@
+## Install powerline fonts
+
+### MacOS
+
+```
+$ git clone https://github.com/powerline/fonts.git --depth=1
+$ cd fonts
+$ ./install.sh
+$ cd ..
+$ rm -rf fonts
+```
+
+change font to "Roboto Mono for Powerline" in iTerm2.
