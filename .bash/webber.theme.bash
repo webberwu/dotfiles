03de9e5251802d66a770ab906eb9abf498b0d41a
Webber Wu <chenshin0719@gmail.com>
2019-01-16 15:19:26 +0800

    modify PS1

--- .bash/webber.theme.bash
+++ .bash/webber.theme.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 # shellcheck disable=SC2034
 
-NORMAL="\\[\\e[0m\\]"
+NORMAL="\[\e[0m\]"
 TERMINAL_BG=16
 ACCOUNT_FG=225
 ACCOUNT_BG=162
@@ -18,16 +18,11 @@ VERSION_CONTROL_BG=106
 function color() {
     # foreground: \[\e[38;5;$1m\]
     # background: \[\e[48;5;$2m\]
-    echo "\\[\\e[38;5;$1m\\]\\[\\e[48;5;$2m\\]"
+    echo "\[\e[38;5;$1m\]\[\e[48;5;$2m\]"
 }
 
-function git_prompt_info() {
-    git_prompt_vars
-    echo -e "$(color ${PATH_BG} ${VERSION_CONTROL_BG})  ${SCM_PREFIX}${SCM_BRANCH}${SCM_STATE}${SCM_SUFFIX} $(color ${VERSION_CONTROL_BG} ${PATH_BG})"
-}
-
-GIT_THEME_PROMPT_DIRTY="$(color 125 ${VERSION_CONTROL_BG}) ✗"
-GIT_THEME_PROMPT_CLEAN="$(color 22 ${VERSION_CONTROL_BG}) ✓"
+GIT_THEME_PROMPT_DIRTY="$(color 125 ${VERSION_CONTROL_BG}) ✗${NORMAL}"
+GIT_THEME_PROMPT_CLEAN="$(color 22 ${VERSION_CONTROL_BG}) ✓${NORMAL}"
 GIT_THEME_PROMPT_PREFIX="$(color ${VERSION_CONTROL_FG} ${VERSION_CONTROL_BG})"
 GIT_THEME_PROMPT_SUFFIX="$(color ${VERSION_CONTROL_FG} ${VERSION_CONTROL_BG})"
 SCM_GIT_SHOW_MINIMAL_INFO=false
@@ -36,10 +31,15 @@ SCM_THEME_PROMPT_CLEAN="${GIT_THEME_PROMPT_CLEAN}"
 SCM_THEME_PROMPT_PREFIX="${GIT_THEME_PROMPT_PREFIX}"
 SCM_THEME_PROMPT_SUFFIX="${GIT_THEME_PROMPT_SUFFIX}"
 
+function git_prompt_info() {
+    git_prompt_vars
+    echo "$(color ${PATH_BG} ${VERSION_CONTROL_BG})  ${SCM_PREFIX}${SCM_BRANCH}${SCM_STATE}${SCM_SUFFIX} $(color ${VERSION_CONTROL_BG} ${PATH_BG})"
+}
+
 function prompt_command() {
-    PS1="${NORMAL}$(color ${ACCOUNT_FG} ${ACCOUNT_BG}) \\u $(color ${ACCOUNT_ARROW_FG} ${ACCOUNT_ARROW_BG})"
-    PS1+="${NORMAL}$(color ${INFO_FG} ${INFO_BG}) \\h[$(color ${INFO_HIGHTLIGHT_FG} ${INFO_BG})\\j$(color ${INFO_FG} ${INFO_BG})] \\t $(color ${INFO_BG} ${PATH_BG})"
-    PS1+="${NORMAL}$(color ${PATH_FG} ${PATH_BG}) \\w $(scm_prompt_info)$(color ${PATH_BG} ${TERMINAL_BG})"
+    PS1="${NORMAL}$(color ${ACCOUNT_FG} ${ACCOUNT_BG}) \u $(color ${ACCOUNT_ARROW_FG} ${ACCOUNT_ARROW_BG})"
+    PS1+="${NORMAL}$(color ${INFO_FG} ${INFO_BG}) \h[$(color ${INFO_HIGHTLIGHT_FG} ${INFO_BG})\j$(color ${INFO_FG} ${INFO_BG})] \t $(color ${INFO_BG} ${PATH_BG})"
+    PS1+="${NORMAL}$(color ${PATH_FG} ${PATH_BG}) \w $(scm_prompt_info)$(color ${PATH_BG} ${TERMINAL_BG})"
     PS1+="${NORMAL} "
 }
 
