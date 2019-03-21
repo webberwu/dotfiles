a73cedf1b58ec6a02c01628893c886271ba8630d
Webber Wu <chenshin0719@gmail.com>
2019-03-21 14:26:53 +0800

    lighten color of PS1

--- .bash/webber.theme.bash
+++ .bash/webber.theme.bash
@@ -5,13 +5,13 @@ NORMAL="\[\e[0m\]"
 TERMINAL_BG=16
 ACCOUNT_FG=225
 ACCOUNT_BG=162
-INFO_FG=246
-INFO_BG=236
+INFO_FG=247
+INFO_BG=238
 INFO_HIGHTLIGHT_FG=208
 ACCOUNT_ARROW_FG="${ACCOUNT_BG}"
 ACCOUNT_ARROW_BG="${INFO_BG}"
 PATH_FG=208
-PATH_BG=235
+PATH_BG=236
 VERSION_CONTROL_FG=15
 VERSION_CONTROL_BG=106
 
@@ -38,7 +38,7 @@ function git_prompt_info() {
 
 function prompt_command() {
     PS1="${NORMAL}$(color ${ACCOUNT_FG} ${ACCOUNT_BG}) \u $(color ${ACCOUNT_ARROW_FG} ${ACCOUNT_ARROW_BG})"
-    PS1+="${NORMAL}$(color ${INFO_FG} ${INFO_BG}) \h[$(color ${INFO_HIGHTLIGHT_FG} ${INFO_BG})\j$(color ${INFO_FG} ${INFO_BG})] \t $(color ${INFO_BG} ${PATH_BG})"
+    PS1+="${NORMAL}$(color ${INFO_FG} ${INFO_BG}) \t [$(color ${INFO_HIGHTLIGHT_FG} ${INFO_BG})\j$(color ${INFO_FG} ${INFO_BG})] $(color ${INFO_BG} ${PATH_BG})"
     PS1+="${NORMAL}$(color ${PATH_FG} ${PATH_BG}) \w $(scm_prompt_info)$(color ${PATH_BG} ${TERMINAL_BG})"
     PS1+="${NORMAL} "
 }
